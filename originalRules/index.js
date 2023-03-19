0482669b8ad4193c068ab19a2611c91993cd9f7b
Jakub Szczepaniak <jakub.szczepaniak@mtab.com>
2023-03-19 18:57:53 +0100

    Removed rules updated.

--- originalRules/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = Object.assign({}, ...[
-  require('./best-practices'),
-  require('./common-js'),
-  require('./es6'),
-  require('./possible-errors'),
-  require('./strict-mode'),
-  require('./stylistic-issues'),
-  require('./variables')
-].map(({ rules }) => rules));
